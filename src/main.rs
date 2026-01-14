4f7d40dbefbfd96869c2a0a8419acc7b3d551707
dragonmux <git@dragonmux.network>
2026-01-13 16:23:42 -0800

    src: Defined a heap for the firmware to use

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,23 @@ mod usb;
 use embassy_executor::Spawner;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::Channel;
+use embedded_alloc::LlffHeap as Heap;
 // Magically inject the parts of the defmt machinary that are needed for doing defmt over RTT 🙃
 use defmt_rtt as _;
 // Magically inject #[panic_handler] so we get panic handling.. don't ask, it's absolutely magic how this can do that.
 use panic_probe as _;
+use static_cell::ConstStaticCell;
 
 use crate::resources::resources::*;
 use crate::serial::serialTask;
 use crate::serial_number::readSerialNumber;
 use crate::types::{ReceiveRequest, TransmitRequest};
 use crate::usb::usbTask;
+
+const HEAP_SIZE: usize = 1024 * 4; // 4KiB heap
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+static HEAP_MEM: ConstStaticCell<[u8; HEAP_SIZE]> = ConstStaticCell::new([0; HEAP_SIZE]);
 
 // Create a pair of channels for moving information between the USB and serial tasks
 static TRANSMIT_CHANNEL: Channel<CriticalSectionRawMutex, TransmitRequest, 1> = Channel::new();
@@ -33,6 +40,13 @@ static RECEIVE_CHANNEL: Channel<CriticalSectionRawMutex, ReceiveRequest, 1> = Ch
 #[embassy_executor::main]
 async fn main(spawner: Spawner)
 {
+	// Initialise our heap so we can use allocating types
+	unsafe
+	{
+		let heapMemory = HEAP_MEM.take() as *mut u8;
+		HEAP.init(heapMemory as usize, HEAP_SIZE);
+	}
+
 	// Initialise the execution environment so we're on the right clock
 	let peripherals = resources::init();
 	let resources = split_resources!(peripherals);
